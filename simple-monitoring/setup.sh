616054a7aabd0c67f7567145566c4a6885cb11e8
warnerb47 <taphagaye2001.mg@gmail.com>
2025-11-17 21:45:33 +0000

    feat(simple-monitoring): add cleanup and setup script

--- simple-monitoring/setup.sh
+++ simple-monitoring/setup.sh
@@ -1,1 +1,22 @@
 #!/bin/sh
+
+# Set colors for output
+RED='\e[1;31m'
+GREEN='\e[1;32m'
+YELLOW='\e[1;33m'
+BLUE='\e[1;34m'
+NC='\e[0m' # No Color
+
+check_is_root() {
+  if [ "$(id -u)" -ne 0 ]; then
+    echo -e "${YELLOW}Warning: setup netdata might require root privileges${NC}"
+  fi
+}
+
+install_netdata() {
+    wget -O /tmp/netdata-kickstart.sh https://get.netdata.cloud/kickstart.sh && sh /tmp/netdata-kickstart.sh
+}
+
+check_is_root
+install_netdata
+
